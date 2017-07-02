8f4d9175008fc6fdf0328bba0d702fb067c39040
atb <al.burns.email@gmail.com>
2017-07-01 22:43:33 -0700

    added comments

--- site.js
+++ site.js
@@ -9,10 +9,11 @@ Every 10 seconds, increase the rate at which moles appear.
 var points = 0;
 var gameTimer;
 var moleSpeed;
-var upMole = [];
-var downMole = [];
-var speedUp = false;
-var game = true;
+var upMole = []; // an array of moles that are popped up
+var downMole = []; // an array of moles that are set to down
+var speedUp = false; // this will be set to true when the game should speed up during the next iteration
+var game = true; // this will be true until the game is over, and all clicking does nothing
+var gameSpeed = 1000; // How often moles will pop up initially
 
 // This part adds every number into the downmole array. The numbers correspond to images of moles.
 // {
@@ -24,7 +25,6 @@ for (i = 1; i <= 20; i++) {
 // This sets an initial gamespeed. when moleSpeed is set, it will run chooseMole at that gamespeed.
 // When gameTimer is set, it chooses how quickly the faster method will increase the speed
 // {
-var gameSpeed = 2000;
 function setTime() {
     moleSpeed = setInterval(chooseMole, gameSpeed);
     gameTimer = setInterval(faster, 10000);
@@ -38,6 +38,9 @@ function faster() {
 // This checks to see if the list of moles down is empty, to see if we have any moles left.
 // If so, it makes a random number between 0 and (array length - 1), to indicate possible indicies of the
 // array that holds the numbers. That changes a random mole's image, and moves them from downmole to upmole.
+// If the boolean "speedup" is true, then it will reset molespeed with a slightly decreased gamespeed.
+// If there are no more moles down, then it will clear all intervals, present a "game over" message,
+// and set "game" to false.
 // {
 function chooseMole() {
     if (downMole.length > 0) {
@@ -50,31 +53,37 @@ function chooseMole() {
         downMole.splice(randMoleIndex, 1);
         if (speedUp) {
             gameSpeed = gameSpeed * .95;
-            moleSpeed = clearInterval();
+            clearInterval(moleSpeed);
             moleSpeed = setInterval(chooseMole, gameSpeed);
             speedUp = false;
         }
     }
     else {
-        moleSpeed = clearInterval();
-        gameTimer = clearInterval();
+        clearInterval(moleSpeed);
+        clearInterval(gameTimer);
         $("#gameover").html("Game over! You got " + points.toString() + " points!");
         game = false;
     }
 }
+// }
 
-$("#points").html(points);
-setTime();
 
+$("#points").html(points); // Show points in the "points" span
+setTime(); // Run everything
 
 
 // This sets the click function for when you click on an item
+// It sets "hitMole" to the mole's number. If hitMole is in the list of moles that are up, and if
+// the game is still running, then the image of that mole is changed, 100 points added, and the
+// mole moves from upmole to downmole.
+// If the mole is down and the game is still running, you lose 50 points.
+// At the end, the "points" html is updated
 // {
 $(".hole").click(function() {
     var hitMole = $(this).attr("id"); // Changed id from int to string, adjust accordingly
     if (upMole.indexOf(hitMole) != -1 && game === true) {
-        var hitIndex = upMole.indexOf(hitMole);
         ($("#" + hitMole)).children().attr('src', 'img/hole.jpg');
+        var hitIndex = upMole.indexOf(hitMole);
         downMole.push(upMole[hitIndex]);
         upMole.splice(parseInt(hitIndex), 1);
         points += 100;
@@ -84,5 +93,5 @@ $(".hole").click(function() {
     }
     $("#points").html(points);
 });
-
+// }
 
